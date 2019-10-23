41fca37c0868620a2f7ad148218dd7db102cbe68
Sun <24259556+sunx2@users.noreply.github.com>
2019-10-23 13:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def hello_world():
     return render_template("index.html",items = b,channel=a)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
